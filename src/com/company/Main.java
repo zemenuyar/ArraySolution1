8dfefb6f094ddc42310c1ac4f3415e958986bb63
zemenuyar <zemenuyar@gmail.com>
2018-10-10 11:48:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,81 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //Create an array to hold the numbers 1-10
+       //1.  int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //  for (int i = 0; i<num.length; i++){
+        num[i] = i + 1;
+
+        //2.  finding the sum of the 1-10.
+        //int sum = 0;
+        //for (int i = 0; i < num.length; i++){
+          //  sum = sum + num[i];
+        //System.out.println(sum);
+        //3. Creat an Array to hold 10 num imput from the user  and the user and print that total to the screen.
+        //Get 10 number iputs from the user , store them in an array and get the averge of those 10 nums
+        totoal = 0;
+
+       // 4  int[]mynumber = new int[10];
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("please enter the number");
+        //int userInputNum =sc.nextByte();
+        //for (int index =0; index<mynumber.length; index++){
+            //System.out.println("please enter the number ");
+            //int userInputLn=sc.nextInt();
+            //mynumber[index]=userInputLn;
+        //5.
+        String[] names = {"Blake", "zed", "jack"};
+        for (int i =0; i<names.length; )
+
+
+                //6.
+        int[] numbers = {2,5,0,2,1,8,5,4};
+        for (int counter = 0; counter<numbers.length; counter++){
+            if (numbers[counter]==5){
+                System.out.println(counter);
+
+                //7.declare an array with the numbers 1-10 backwards from the array.
+                int [] num = {1,2,3,4,5,6,7,8,9,10};
+                for (int i=num.length-1; i>=0; i--){
+                    System.out.println(num[i]);
+                    //8. given the following arrays: ["w", "t", "h", "k"]
+                    //loop through the array replace the letter 't' with the world "hello" once it`s found
+
+                    String []letters = {"w", "t","y","h", "k"};
+                    for (int i = 0; i < letters.length; i++){
+                        if(letters[i].equalsIgnoreCase("t") {
+                            letters[i] = "hello";
+
+                            System.out.println(letters[i]);
+
+                            // given array 1:[1,7,6,9] and array 2:[2,7,6,4] write a program that will print all maching
+                            //pairss from array 1 and 2
+                            int[] num = {1,7,5,9};
+                            int[] num2 = {2,7,6,4};
+                            for (int counter = 0; counter<num.length; counter++){
+                                for (int counter =0; counter<num2.length; counter++){
+                                    if (num[counter]==num2[counter]){
+                                        System.out.println(num[counter] + " ," + num2[counter]);
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+
+
+                }
+            }
+
+
+        }
+
+
+    }
+}
+
